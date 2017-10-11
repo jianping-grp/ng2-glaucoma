c9d03211aafe20106f9405726cb1c1a5046d491b
zhonghua-wang <zhonghua.wang@outlook.com>
2017-10-11 23:55:10 +0800

    upgrade angular to 4.4.4

--- src/app/service/rest/rest.service.ts
+++ src/app/service/rest/rest.service.ts
@@ -35,12 +35,13 @@ export class RestService {
       .finally(() => this.globalService.setLoading(false));
   }
 
-  postCompoundByStructure(smiles:string): Observable<any>{
-    const body= {smiles: smiles, similarity: 0.5, substructure_search: 0};
+  postCompoundByStructure(smiles: string): Observable<any>{
+    const data= {smiles: smiles, similarity: 0.5, substructure_search: 0};
     this.globalService.setLoading(true);
-    return this.http.post(`${this.REST_HOST}/compounds/search/`,body)
+    console.log({data});
+    return this.http.post(`${this.REST_HOST}/compounds/search/`, data)
       .finally(() => this.globalService.setLoading(false))
-      .catch(this.handleError)
+      // .catch(this.handleError)
   }
 
   postCompoundBySubstructure(smiles:string): Observable<any> {
